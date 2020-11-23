7e7c392ce98c0359154e309855ce4dfef6fd5080
HimanshuMittal01 <44058642+HimanshuMittal01@users.noreply.github.com>
2020-11-23 13:45:15 +0530

    installed qiskit

---
+++ app/qcalgos.py
@@ -0,0 +1,6 @@
+from qiskit import QuantumCircuit, execute, Aer
+from qiskit.visualization import plot_histogram, plot_bloch_multivector
+from numpy.random import randint
+import numpy as np
+
+print("Imports Successful")
